f74b32c9fedd12500a92d567c4638b0512c64f73
Shatil RAHMAN <srahma34@ds2.CS.McGill.CA>
2018-01-25 14:39:00 -0500

    wrote test.py to read from csv into array, plotted array

--- test.py
+++ test.py
@@ -4,4 +4,24 @@ Name: Shatil Rahman
 ID: 260606042
 '''
 
-print "hello world"
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+
+feature_1 = []
+output_1 = []
+
+with open('Datasets/Dataset_1_test.csv', 'rb') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        x = float(row[0])
+        y = float(row[1])
+        feature_1.append(x)
+        output_1.append(y)
+        
+X = np.array(feature_1)
+Y = np.array(output_1)
+
+plt.plot(X,Y,'ro')
+plt.show
+    
