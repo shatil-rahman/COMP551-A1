aefe447ef7201aed45fa2bd5a5e70327d6a0a5f4
Shatil RAHMAN <srahma34@ds2.CS.McGill.CA>
2018-01-28 12:21:54 -0500

    Tried to clean up the code by encapsulating things in functions

--- Q3.py
+++ Q3.py
@@ -22,22 +22,26 @@ def MSE(W,X,Y):
     MSE = ((error**2).sum())/(error.size)
     return MSE
  
-'''   
-def k_fold_validate(W, val_sets):
-    MSE_list = []
-    for example in val_sets:
-        MSE_list.append(MSE(W,example[0], example[1]))
+def loadData(examples_train, examples_test):
+    for i in range(1,6):
+        s_train = "Datasets/CandC-train" + str(i) + ".csv"
+        s_test = "Datasets/CandC-test" + str(i) + ".csv"
+        
+        X = np.loadtxt(s_train, dtype=float, delimiter=',', ndmin=2)
+        o = np.ones(X.shape[0]).reshape(X.shape[0],1)
+        X = np.concatenate((o,X), axis=1)
+        examples_train.append([X[:,:-1], X[:,-1].reshape(X.shape[0],1)])
+        
+        X2 = np.loadtxt(s_test, dtype=float, delimiter=',', ndmin=2)
+        o = np.ones(X2.shape[0]).reshape(X2.shape[0],1)
+        X2 = np.concatenate((o,X2), axis=1)
+        examples_test.append([X2[:,:-1], X2[:,-1].reshape(X2.shape[0],1)])
     
-    return MSE_list
-''' 
+        
 
 
-
-############Load Data #####################
-#train1 = np.loadtxt('Datasets/CandC-train1.csv', dtype=float, delimiter=',', ndmin=2)
-#X_train1 = train1[:,:-1]
-#Y_train1 = train1[:,-1].reshape(train1.shape[0],1)
-
+#############################Load Data #######################################
+'''
 examples_train = []
 examples_test = []
 
@@ -54,20 +58,44 @@ for i in range(1,6):
     o = np.ones(X2.shape[0]).reshape(X2.shape[0],1)
     X2 = np.concatenate((o,X2), axis=1)
     examples_test.append([X2[:,:-1], X2[:,-1].reshape(X2.shape[0],1)])
-    
+'''  
     
 ##########################Training the models #################################
+
+examples_train = []
+examples_test = []
+
+loadData(examples_train, examples_test)
+
+Avg_MSE = []
+Lambda = 1.2
+Lambda_list = []
+
+for j in range(0,20):
+    Models = []
+    MSE_list = []
+    for i in range(0,5):
+        example = examples_train[i]
+        test_example = examples_test[i]
+        
+        Models.append(train(example[0], example[1], Lambda))
+        MSE_test = MSE(Models[i], test_example[0], test_example[1])
+        MSE_list.append(MSE_test)
+        #print "MSE for Set 1: " + str(MSE_test)
+        
+    MSE_list = np.array(MSE_list)
+    Avg_MSE.append(np.average(MSE_list))
+    Lambda_list.append(Lambda)
+    Lambda = Lambda + 0.01
+    
+plt.figure(figsize=[10,10])
+plt.plot(Lambda_list, Avg_MSE, 'b-')
+plt.show
+
+
     
 
-Models = []
 
-for i in range(0,5):
-    example = examples_train[i]
-    test_example = examples_test[i]
-    
-    Models.append(train(example[0], example[1], 0.0))
-    MSE_test = MSE(Models[i], test_example[0], test_example[1])
-    print "MSE for Set 1: " + str(MSE_test)
 
 
 
