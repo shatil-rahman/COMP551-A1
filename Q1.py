Linear Regression with L2 Regularization for Communities and Crime dataset, fixed plots in Stochastic Gradient Descent question

Q1.py
@@ -10,14 +10,14 @@ import matplotlib.pyplot as plt
 
 
 #importing the data
-x = np.genfromtxt('Datasets/Dataset_1_train.csv', dtype=float, delimiter=",", usecols=(0))
-Y = np.genfromtxt('Datasets/Dataset_1_train.csv', dtype=float, delimiter=",", usecols=(1))
+x = np.genfromtxt('Datasets/Dataset_1_train.csv', delimiter=",", usecols=(0))
+Y = np.genfromtxt('Datasets/Dataset_1_train.csv', delimiter=",", usecols=(1))
 
-x_val = np.genfromtxt('Datasets/Dataset_1_valid.csv', dtype=float, delimiter=",", usecols=(0))
-Y_val = np.genfromtxt('Datasets/Dataset_1_valid.csv', dtype=float, delimiter=",", usecols=(1))
+x_val = np.genfromtxt('Datasets/Dataset_1_valid.csv', delimiter=",", usecols=(0))
+Y_val = np.genfromtxt('Datasets/Dataset_1_valid.csv', delimiter=",", usecols=(1))
 
-x_test = np.genfromtxt('Datasets/Dataset_1_test.csv', dtype=float, delimiter=",", usecols=(0))
-Y_test = np.genfromtxt('Datasets/Dataset_1_test.csv', dtype=float, delimiter=",", usecols=(1))
+x_test = np.genfromtxt('Datasets/Dataset_1_test.csv', delimiter=",", usecols=(0))
+Y_test = np.genfromtxt('Datasets/Dataset_1_test.csv', delimiter=",", usecols=(1))
 
 ################### Model Selection and Regularization #######################
 
@@ -48,21 +48,21 @@ W = np.dot(Xt_X_inv, Xt_Y)
 #Training set MSE
 y_training = np.polynomial.polynomial.polyval(x,W)
 error = Y - y_training 
-MSE = np.linalg.norm(error)
+MSE = ((error**2).sum())/(error.size)
 
 print "Mean Squared Error on Training Set: " + str(MSE)
 
 #Validation set MSE
 Y_val_predicted = np.polynomial.polynomial.polyval(x_val, W)
 error_val = Y_val - Y_val_predicted
-MSE_val = np.linalg.norm(error_val)
+MSE_val = ((error_val**2).sum())/(error_val.size)
 
 print "Mean Squared Error on Validation Set: " + str(MSE_val)
 
 #Test set MSE
 Y_test_predicted = np.polynomial.polynomial.polyval(x_test, W)
 error_test = Y_test - Y_test_predicted
-MSE_test = np.linalg.norm(error_test)
+MSE_test = ((error_test**2).sum())/(error_test.size)
 
 print "Mean Squared Error on Test Set: " + str(MSE_test)
 
@@ -77,6 +77,7 @@ plt.subplot(311)
 plt.plot(x,Y,'rx')
 plt.plot(x_fit,Y_fit, 'b-')
 plt.axis([-1.5, 1.5, -20.0, 35.0])
+plt.title('Linear Regression by SGD on Training Data')
 plt.legend(['Training data', 'model'], loc=2)
 
 
@@ -85,12 +86,15 @@ plt.subplot(312)
 plt.plot(x_val,Y_val,'rx')
 plt.plot(x_fit,Y_fit, 'b-')
 plt.axis([-1.5, 1.5, -20.0, 35.0])
+plt.title('Linear Regression by SGD on Validation Data')
 plt.legend(['Validation data', 'model'], loc=2)
 
-#Plotting validation results
+#Plotting test results
 plt.subplot(313)
 plt.plot(x_test,Y_test,'rx')
 plt.plot(x_fit,Y_fit, 'b-')
 plt.axis([-1.5, 1.5, -20.0, 35.0])
-plt.legend(['Validation data', 'model'], loc=2)
+plt.title('Linear Regression by SGD on Test Data')
+plt.legend(['Test data', 'model'], loc=2)
 plt.show
+

test.py
@@ -22,6 +22,6 @@ with open('Datasets/Dataset_1_test.csv', 'rb') as csvfile:
 X = np.array(feature_1)
 Y = np.array(output_1)
 
-plt.plot(X,Y,'ro')
+plt.plot(X,Y,'bx')
 plt.show
     